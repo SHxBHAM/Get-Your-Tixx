53ef098f5e5f69191eede48862093127ebabd459
SHxBHAM <shubhammishra41880@gmail.com>
2024-05-02 12:54:18 +0530

    cloud-DB

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ require('dotenv').config();
 const app = express();
 const PORT = 3000;
 
-//MongoDB setup
-// mongoose.connect('mongodb://localhost:27017/login_register_db', { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', () => {
-//     console.log('Connected to MongoDB');
-// });
+// MongoDB setup
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
 
 // Schema
 const userSchema = new mongoose.Schema({
